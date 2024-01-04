608b3739c281bc05c9416746851abdc531f24380
Luiza Closs <luizacloss.2205@gmail.com>
2024-01-04 12:31:42 -0300

    04102023

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,40 +1,108 @@
 import { useEffect, useState } from "react";
 import { Container, Movie, MovieList, Btn } from "./style";
 import { Link } from "react-router-dom";
+import { Carousel } from "bootstrap";
 
 function Home() {
-    const imagePath = "https://image.tmdb.org/t/p/w500";
-    const [movies, setMovies] = useState([]);
+    const imagePath = "https://image.tmdb.org/t/p/original";
+    const [movies, setMovies] = useState([])
+    const [popular, setPopular] = useState([])
+    const [news, setNew] = useState([])
+    const [top, setTops] = useState([])
     const KEY = process.env.REACT_APP_KEY;
-    const Pages = 15;
     
-    useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${KEY}&language=pt-BR`)
+    useEffect(() =>{
+        async function carregarLista(){
+            fetch(`https://api.themoviedb.org/3/list/8271861?api_key=${KEY}&language=pt-BR`)
+                .then(response => response.json())
+                .then((data) => {
+                    setMovies(data.items)
+                    loadPop();
+                    loadNew();
+                    loadTop();
+                })
+        }
+        async function loadPop(){
+            fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${KEY}&language=pt-BR`)
             .then((response) => response.json())
             .then((data) => {
-                setMovies(data.results);
+                setPopular(data.results);
             });
-    }, [KEY]);
+        }
+        async function loadNew(){
+            fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${KEY}&language=pt-BR`)
+            .then((response) => response.json())
+            .then((data) => {
+                setNew(data.results);
+            });
+        }
+        async function loadTop() {
+            fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${KEY}&language=pt-BR`)
+                .then((response) => response.json())
+                .then((data) => {
+                    setTops(data.results);
+                });
+        }
 
-
+        carregarLista();
+    }, [ KEY ])
 
     return (
         <div className="container">
-            <div>
-                <div className="row mt-5">
-                    {movies.map((filme) => (
-                        <div key={filme.id} className="col-md-2 md-3">
-                            <Link to={`${filme.id}`}> 
-                                <img
-                                    src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`}
-                                    alt={filme.title}
-                                    className="img-fluid poster"
-                                />
-                                <span>{filme.title}</span>
-                            </Link>   
-                        </div>
-                    ))}
-                </div>
+            <div className="row">
+                {movies.map((filme) => (
+                    <div key={filme.id} className="col-md-3 md-2 p-1">
+                        <Link to={`${filme.id}`}>
+                            <img
+                                src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`}
+                                alt={filme.title}
+                                className="img-fluid poster"
+                            />
+                        </Link>
+                    </div>
+                ))}
+            </div>
+            <h1>Filmes Populares</h1>
+            <div className="row">
+                {popular.map((filme) => (
+                    <div key={filme.id} className="col-md-3 md-2 p-1">
+                        <Link to={`${filme.id}`}>
+                            <img
+                                src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`}
+                                alt={filme.title}
+                                className="img-fluid poster"
+                            />
+                        </Link>
+                    </div>
+                ))}
+            </div>
+            <h1>Acabaram de Chegar</h1>
+            <div className="row">
+                {news.map((filme) => (
+                    <div key={filme.id} className="col-md-3 md-2 p-1">
+                        <Link to={`${filme.id}`}>
+                            <img
+                                src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`}
+                                alt={filme.title}
+                                className="img-fluid poster"
+                            />
+                        </Link>
+                    </div>
+                ))}
+            </div>
+            <h1>Filmes Inesqueciveis</h1>
+            <div className="row">
+                {top.map((filme) => (
+                    <div key={filme.id} className="col-md-3 md-2 p-1">
+                        <Link to={`${filme.id}`}>
+                            <img
+                                src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`}
+                                alt={filme.title}
+                                className="img-fluid poster"
+                            />
+                        </Link>
+                    </div>
+                ))}
             </div>
         </div>
     );
--- src/pages/movie/index.js
+++ src/pages/movie/index.js
@@ -5,35 +5,21 @@ import "./styles.css";
 
 const Movie = () => {
   const { id } = useParams();
-  const imagePath = "https://image.tmdb.org/t/p/w500";
-
-  const [trailerKey, setTrailerKey] = useState("");
+  const imagePath = "https://image.tmdb.org/t/p/original";
+  
   const [movie, setMovie] = useState([]);
 
   const KEY = process.env.REACT_APP_KEY;
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${KEY}&language=pt-BR`
-    )
-      .then((response) => response.json())
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${KEY}&language=pt-BR`)
+      .then((response) => 
+        response.json()
+      )
       .then((data) => {
-        const res = data.results;
-        let filme = res.find((key) => {
-          // eslint-disable-next-line
-          return key.id == id;
-        });
-        setMovie(filme);
-      }); // eslint-disable-next-line
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${KEY}&language=pt-BR`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const videos = data.results;
-        const trailer = videos.find((video) => video.type === "Trailer");
-        if (trailer) {
-          setTrailerKey(trailer.key);
-        }
+        setMovie(data); // Ajuste aqui para usar diretamente o objeto retornado pela API
+      })
+      .catch((error) => {
+        //console.error("Erro ao buscar detalhes do filme:", error);
       });
   }, [id, KEY]);
 
@@ -44,61 +30,49 @@ const Movie = () => {
       return;
     }
 
-    let estrelas = (valor * 5) / 10;
+    let estrelasPreenchidas = Math.floor((valor * 5) / 10);
+    let estrelasVazias = 5 - estrelasPreenchidas;
+
     let resultado = "";
 
-    for (let i = 0; i < estrelas; i++) {
-      if (i < 5) {
-        resultado += "★"; // Use o caractere que representa o ícone aqui
-      }
+    // Adiciona estrelas preenchidas (vermelhas)
+    for (let i = 0; i < estrelasPreenchidas; i++) {
+      resultado += "★";
     }
 
+    // Adiciona estrelas vazias (azuis)
+    for (let i = 0; i < estrelasVazias; i++) {
+      resultado += "☆";
+    }
     return resultado;
   }
 
   return (
-    <div>
-      <div className="container">
-        <div className="container mt-4">
-          <div className="card-img-top" width="100%">
-            <img
-              width="100%"
-              className="img_movie"
-              src={`${imagePath}${movie.backdrop_path}`}
-              alt="{movie.title}"
-            />
-          </div>
-          <div className="card-body">
-            <h3 className=" card-text">{movie.title}</h3>
-            <p className="card-text">{movie.overview}</p>
-            <p className="card-text">
-              {criarIcones(movie.vote_average)} {movie.vote_average}
-            </p>
-            <div className="d-flex justify-content-between align-items-center">
-            
+    <div style={{ backgroundImage: `url(${imagePath}${movie.backdrop_path})`}} className="coisotodo">
+      <p>.</p>
+      <div className="maincard">
+        <div className="Banner">
+          <img 
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}/>
+        </div>
+        <div className="cardizin">
+          <div className="parte1">
+            <div className="vazioInfinito"></div>
+            <div className="textinhoUm">
+              <h4> {criarIcones(movie.vote_average)}</h4>
+              <p className="small"> {movie.vote_average} </p>
             </div>
+          </div>
+          <div className="textin">
+            <h1> <strong> {movie.title} </strong></h1>
+            <p>{movie.release_date}</p>
+            <p>{movie.overview}</p>
           </div>
         </div>
       </div>
+
     </div>
-    /*<div>
-            <img
-                className="img_movie"
-                src={`${imagePath}${movie.poster_path}`}
-                alt="{movie.title}"
-            />
-            <div className="container">
-                <h1>{movie.title}</h1>
-                <h3>Data de lançamento: {movie.release_date}</h3>
-                <div className="descricao">
-                    <h4>Descrição: </h4>
-                    <p className="movie-desc">{movie.overview}</p>
-                </div>
-                <Link to="/">
-                    <button className="link_button">Voltar</button>
-                </Link>
-            </div>
-        </div>*/
   );
 };
 
